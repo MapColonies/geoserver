3bcd3b9af2ab155c00396523902a4057ea4ef66c
Niv Greenstein <88280771+NivGreenstein@users.noreply.github.com>
2025-07-28 14:47:59 +0300

    Revert "feat: geoserver image upgrade"

--- index.mjs
+++ index.mjs
@@ -1,31 +1,34 @@
 #!/usr/bin/env zx
 
 const {
-	GEOSERVER_VERSION = '2.27.1--v2025.07.17',
-	IMAGE_DOCKER_REGISTRY,
-	IMAGE_REPO = 'geoserver',
-	WORK_DIR = '/tmp/geoserver',
+  GEOSERVER_VERSION = '2.26.0',
+  IMAGE_DOCKER_REGISTRY,
+  IMAGE_REPO = 'geoserver',
+  WORK_DIR = '/tmp/geoserver',
+  OTEL_VERSION = 'v2.9.0',
+  LOG4J_VERSION = '2.17.2',
+  JMX_PROMETHEUS_VERSION = '0.19.0',
 } = process.env;
 
 try {
-	const packageVersion = await require('./package.json').version;
-	const imageName = `${IMAGE_REPO}:v${packageVersion}-${GEOSERVER_VERSION}`;
-	const geoserverBaseImageName = `kartoza/geoserver:${GEOSERVER_VERSION}`;
+  const packageVersion = await require('./package.json').version;
+  const imageName = `${IMAGE_REPO}:v${packageVersion}-${GEOSERVER_VERSION}`;
+  const geoserverBaseImageName = `kartoza/geoserver:${GEOSERVER_VERSION}`;
 
-	await $`docker build -q --build-arg GEOSERVER_BASE_IMAGE=${geoserverBaseImageName} -f Dockerfile -t ${imageName} .`;
+  await $`docker build -q --build-arg OTEL_VERSION=${OTEL_VERSION} --build-arg LOG4J_VERSION=${LOG4J_VERSION} --build-arg JMX_PROMETHEUS_VERSION=${JMX_PROMETHEUS_VERSION} --build-arg GEOSERVER_BASE_IMAGE=${geoserverBaseImageName} -f Dockerfile -t ${imageName} .`;
 
-	console.log(chalk.blue('Builds Openshift ready Geoserver Image'));
-	console.log(IMAGE_DOCKER_REGISTRY);
+  console.log(chalk.blue('Builds Openshift ready Geoserver Image'));
+  console.log(IMAGE_DOCKER_REGISTRY);
 
-	if (IMAGE_DOCKER_REGISTRY) {
-		const taggedImageName = `${IMAGE_DOCKER_REGISTRY}/${imageName}`;
-		await $`docker tag ${imageName} ${taggedImageName}`;
-		console.log(chalk.blue(`Tagged Docker Image as ${taggedImageName}`));
-	}
-	console.log(chalk.magenta('We did it!! 🐧🐧🐧🐧🐧'));
+  if (IMAGE_DOCKER_REGISTRY) {
+    const taggedImageName = `${IMAGE_DOCKER_REGISTRY}/${imageName}`;
+    await $`docker tag ${imageName} ${taggedImageName}`;
+    console.log(chalk.blue(`Tagged Docker Image as ${taggedImageName}`));
+  }
+  console.log(chalk.magenta('We did it!! 🐧🐧🐧🐧🐧'));
 } catch (e) {
-	console.log(chalk.red('Oh no! 😢'));
-	console.error(e);
+  console.log(chalk.red('Oh no! 😢'));
+  console.error(e);
 } finally {
-	await $`rm -rf ${WORK_DIR}`;
+  await $`rm -rf ${WORK_DIR}`;
 }
